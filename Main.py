efec7fca213bcece41d1861a617fb43d2616d0e0
Jack Owens <40874726+rjackowens@users.noreply.github.com>
2018-12-13 13:52:20 -0600

    Update Main.py
    Added price filtering logic and prevented duplicate item alerting.

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import json
 import requests
 import boto3
-from config import key, number 
+from config import key, number
 from CalculateAverage import calculate_average
 from colorama import init, Fore
 init()
 
-# # Function to Calculate Average
+# Calculate Average
 # def calculate_average(x):
 #    return sum(x) / float(len(x))
 
@@ -18,13 +18,13 @@ print ("\n1 = New \n2 = Used \n3 = For Parts or Not Working")
 
 condition = input("\nSelect Condition: ")
 if condition == "1":
-  condition = "1000"
+ condition = "1000"
 elif condition == "2":
-  condition = "3000"
+ condition = "3000"
 elif condition == "3":
-  condition = "7000"
+ condition = "7000"
 else:
-  print ("You did not select a valid category")
+ print ("You did not select a valid category")
 
 # API Request for Sold Listings
 sold_url = ("http://svcs.ebay.com/services/search/FindingService/v1\
@@ -68,7 +68,7 @@ active_url = ("http://svcs.ebay.com/services/search/FindingService/v1\
 &itemFilter(3).name=ListingType\
 &itemFilter(3).value(0)=AuctionWithBIN\
 &itemFilter(3).value(1)=FixedPrice\
-&paginationInput.entriesPerPage=10\
+&paginationInput.entriesPerPage=100\
 &sortOrder=PricePlusShippingLowest\
 &keywords=" + searchTerm)
 
@@ -77,13 +77,12 @@ active_raw = active_results.json()
 
 # Displays Average Sold Price
 new_list1=[]
-
 print (Fore.YELLOW + "\nAverage Sold Price: \n" + Fore.GREEN)
 for item in (sold_raw["findCompletedItemsResponse"][0]["searchResult"][0]["item"]):
-  price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
-  title = item["title"][0]
-  int_price = int(float(price))
-  new_list1.append(int_price)
+ price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+ title = item["title"][0]
+ int_price = int(float(price))
+ new_list1.append(int_price)
 
 average_sold_price = (sum(new_list1) / float(len(new_list1)))
 print (average_sold_price)
@@ -91,13 +90,12 @@ print ("") # New Line
 
 # Displays Average Active Price
 new_list2=[]
-
 print (Fore.YELLOW + "Average Active Price: \n" + Fore.GREEN)
 for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
-  price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
-  title = item["title"][0]
-  int_price = int(float(price))
-  new_list2.append(int_price)
+ price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+ title = item["title"][0]
+ int_price = int(float(price))
+ new_list2.append(int_price)
 
 average_active_price = (calculate_average(new_list2))
 print (average_active_price)
@@ -105,34 +103,48 @@ print (average_active_price)
 # Displays Recent Sold Listings
 print (Fore.YELLOW + "\nRecent Sold Listings: \n" + Fore.GREEN)
 for item in (sold_raw["findCompletedItemsResponse"][0]["searchResult"][0]["item"]):
-  title = item["title"][0]
-  price2 = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
-  print (title + " $" + price2)
+ title = item["title"][0]
+ url = item["viewItemURL"][0]
+ price2 = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+ print (title + ":" + Fore.YELLOW + " $"  + price2 + " " + Fore.BLUE + url + Fore.GREEN)
 
 # Displays Current Active Listings
 print (Fore.YELLOW + "\nActive Listings: \n" + Fore.GREEN)
 for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
-   price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
-   title = item["title"][0]
-   url = item["viewItemURL"][0]
-   print (title + " $" + price + " \nLink: " + url + "\n")
-   int_price = int(float(price))
-   new_list1.append(int_price)
+  price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+  title = item["title"][0]
+  url = item["viewItemURL"][0]
+  print (title + ":" + Fore.YELLOW + " $"  + price + " " + Fore.BLUE + url + Fore.GREEN)
+  int_price = int(float(price))
+  new_list1.append(int_price)
 
-# Send Average Item Price Text Messages via AWS SNS 
+# Imports Previously Alerted Items
+with open("itemid.txt", "r") as item_file:
+   itemids = item_file.read().splitlines()
+
+# Init AWS SNS
 aws_client = boto3.client("sns")
 sns_phone_number = number
 
-active_message = "The average active price of " + searchTerm + " is $" + str(average_active_price)
-
-aws_response = aws_client.publish(
-    PhoneNumber=sns_phone_number,
-    Message=(active_message)
-)
-
-sold_message = "The average sold price of " + searchTerm + " is $" + str(average_sold_price)
-
-aws_response = aws_client.publish(
-    PhoneNumber=sns_phone_number,
-    Message=(sold_message)
-)
+# Text Message Alerting + Item Filtering
+print (Fore.YELLOW + "\nListings to Alert: \n" + Fore.GREEN)
+for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
+ item_id = item["itemId"][0]
+ price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+ title = item["title"][0]
+ url = item["viewItemURL"][0]
+ int_price = int(float(price))
+ if int_price < average_sold_price and item_id not in itemids: # Removes previously alerted items and items over the average sold price
+   print(title + ":" + Fore.YELLOW + " $"  + price + " " + Fore.BLUE + url + Fore.GREEN)   
+  
+   # Send AWS Text Alert
+   sns_message = (title + ":" + " $"  + price + " " + url)
+   aws_response = aws_client.publish(
+     PhoneNumber=sns_phone_number,
+     Message=sns_message,
+  )
+  # Adds Items to Previously Alerted Items 
+   with open("itemid.txt", "a") as item_file:
+     item_file.write((item_id + "\n"))
+ else:
+  continue
