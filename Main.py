f1f8b7df9ecdab29dab1a21b33609a1d2746926f
Jack Owens <40874726+rjackowens@users.noreply.github.com>
2018-12-21 12:06:09 -0600

    Improved error handling

--- Main.py
+++ Main.py
@@ -2,18 +2,13 @@ import json
 import requests
 import boto3
 from config import key, number
-from CalculateAverage import calculate_average
 from colorama import init, Fore
 init()
 
-# Calculate Average
-# def calculate_average(x):
-#    return sum(x) / float(len(x))
-
 # Prompts User to Enter Search Term, Min/Max Price, and Condition
-searchTerm = input ("\nEnter Search Term: ")
-minPrice = input("Enter Minimum Price: ") # Enter %00 to hard code no min price
-maxPrice = input("Enter Maximum Price: ") # Enter %00 to hard code no max price
+search_term = input ("\nEnter Search Term: ")
+min_price = input("Enter Minimum Price: ") # Enter %00 to hard code no min price
+max_price = input("Enter Maximum Price: ") # Enter %00 to hard code no max price
 
 print ("\n1 = New \n2 = Used \n3 = For Parts or Not Working")
 
@@ -40,17 +35,23 @@ sold_url = ("http://svcs.ebay.com/services/search/FindingService/v1\
 &itemFilter(2).name=SoldItemsOnly\
 &itemFilter(2).value=true\
 &itemFilter(3).name=MinPrice\
-&itemFilter(3).value=" + minPrice +"&itemFilter(1).paramName=Currency\
+&itemFilter(3).value=" + min_price +"&itemFilter(1).paramName=Currency\
 &itemFilter(3).paramValue=USD\
 &itemFilter(4).name=MaxPrice\
-&itemFilter(4).value=" + maxPrice +"&itemFilter(2).paramName=Currency\
+&itemFilter(4).value=" + max_price +"&itemFilter(2).paramName=Currency\
 &itemFilter(4).paramValue=USD\
 &sortOrder=PricePlusShippingLowest\
 &paginationInput.entriesPerPage=100\
-&keywords=" + searchTerm)
+&keywords=" + search_term)
 
 sold_results = requests.get(sold_url)
-sold_raw = sold_results.json()
+# sold_raw = sold_results.json()
+
+try:
+  sold_raw = sold_results.json()
+except ValueError: #json.decoder.JSONDecodeError
+  print (Fore.RED + "\nNo Listings Found. JSONDecodeError has Occured." + Fore.RESET)
+  #pass # Throws exception at line 75
 
 # API Request for Active Listings
 active_url = ("http://svcs.ebay.com/services/search/FindingService/v1\
@@ -61,69 +62,92 @@ active_url = ("http://svcs.ebay.com/services/search/FindingService/v1\
 &itemFilter(0).name=Condition\
 &itemFilter(0).value=" + condition + "\
 &itemFilter(1).name=MinPrice\
-&itemFilter(1).value=" + minPrice +"&itemFilter(1).paramName=Currency\
+&itemFilter(1).value=" + min_price +"&itemFilter(1).paramName=Currency\
 &itemFilter(1).paramValue=USD\
 &itemFilter(2).name=MaxPrice\
-&itemFilter(2).value=" + maxPrice +"&itemFilter(2).paramName=Currency\
+&itemFilter(2).value=" + max_price +"&itemFilter(2).paramName=Currency\
 &itemFilter(2).paramValue=USD\
 &itemFilter(3).name=ListingType\
 &itemFilter(3).value(0)=AuctionWithBIN\
 &itemFilter(3).value(1)=FixedPrice\
 &paginationInput.entriesPerPage=100\
 &sortOrder=PricePlusShippingLowest\
-&keywords=" + searchTerm)
+&keywords=" + search_term)
 
 active_results = requests.get(active_url)
-active_raw = active_results.json()
+# active_raw = active_results.json()
+
+try:
+  active_raw = active_results.json()
+except ValueError: #json.decoder.JSONDecodeError
+  print (Fore.RED + "\nNo Listings Found. JSONDecodeError has Occured." + Fore.RESET)
 
 # Displays Average Sold Price
 new_list1=[]
 print (Fore.YELLOW + "\nAverage Sold Price: \n" + Fore.GREEN)
 
-for item in (sold_raw["findCompletedItemsResponse"][0]["searchResult"][0]["item"]):
- price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
- title = item["title"][0]
- int_price = int(float(price))
- new_list1.append(int_price)
+try:
+  for item in (sold_raw["findCompletedItemsResponse"][0]["searchResult"][0]["item"]):
+    price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+    title = item["title"][0]
+    int_price = int(float(price))
+    new_list1.append(int_price)
+except KeyError:
+  print (Fore.RED + "\nNo Listings Found. KeyError Has Occured." + Fore.RESET)
 
-average_sold_price = (sum(new_list1) / float(len(new_list1)))
-
-print (average_sold_price)
-print ("") # New Line
+try:
+  def calculate_average(x):
+    return sum(x) / float(len(x))
+  average_sold_price = (calculate_average(new_list1))
+  print (average_sold_price)
+  print ("") # New Line
+except ZeroDivisionError:
+  print (Fore.RED + "\nNo Listings Found. ZeroDivisionError Has Occured." + Fore.RESET)
 
 # Displays Average Active Price
 new_list2=[]
 print (Fore.YELLOW + "Average Active Price: \n" + Fore.GREEN)
 
-for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
- price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
- title = item["title"][0]
- int_price = int(float(price))
- new_list2.append(int_price)
+try:
+  for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
+    price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+    title = item["title"][0]
+    int_price = int(float(price))
+    new_list2.append(int_price)
+except KeyError:
+  print (Fore.RED + "\nNo Listings Found. KeyError Has Occured." + Fore.RESET)
 
-average_active_price = (calculate_average(new_list2))
+try:
+  average_active_price = (calculate_average(new_list2))
+  print (average_active_price)
+except ZeroDivisionError:
+  print (Fore.RED + "\nNo Listings Found. ZeroDivisionError Has Occured." + Fore.RESET)
 
-print (average_active_price)
+###################################
 
 # Displays Recent Sold Listings
 print (Fore.YELLOW + "\nRecent Sold Listings: \n" + Fore.GREEN)
 
-for item in (sold_raw["findCompletedItemsResponse"][0]["searchResult"][0]["item"]):
- title = item["title"][0]
- url = item["viewItemURL"][0]
- price2 = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
- print (title + ":" + Fore.YELLOW + " $"  + price2 + " " + Fore.BLUE + url + Fore.GREEN)
+try:
+  for item in (sold_raw["findCompletedItemsResponse"][0]["searchResult"][0]["item"]):
+    title = item["title"][0]
+    url = item["viewItemURL"][0]
+    price2 = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+    print (title + ":" + Fore.YELLOW + " $"  + price2 + " " + Fore.BLUE + url + Fore.GREEN)
+except KeyError:
+  print (Fore.RED + "\nNo Listings Found. KeyError Has Occured." + Fore.RESET)
 
 # Displays Current Active Listings
 print (Fore.YELLOW + "\nActive Listings: \n" + Fore.GREEN)
 
-for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
-  price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
-  title = item["title"][0]
-  url = item["viewItemURL"][0]
-  print (title + ":" + Fore.YELLOW + " $"  + price + " " + Fore.BLUE + url + Fore.GREEN)
-  int_price = int(float(price))
-  new_list1.append(int_price)
+try:
+  for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]):
+    price = item["sellingStatus"][0]["convertedCurrentPrice"][0]['__value__']
+    title = item["title"][0]
+    url = item["viewItemURL"][0]
+    print (title + ":" + Fore.YELLOW + " $"  + price + " " + Fore.BLUE + url + Fore.GREEN)
+except KeyError:
+  print (Fore.RED + "\nNo Listings Found. KeyError Has Occured." + Fore.RESET)
 
 # Imports Previously Alerted Items
 with open("itemid.txt", "r") as item_file:
@@ -152,6 +176,7 @@ for item in (active_raw["findItemsByKeywordsResponse"][0]["searchResult"][0]["it
      PhoneNumber=sns_phone_number,
      Message=sns_message,
   )
+
   # Adds Items to Previously Alerted Items 
    with open("itemid.txt", "a") as item_file:
      item_file.write((item_id + "\n"))
